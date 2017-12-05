e438a9bbe6fea0d5a7db110cdaa839140be00ec9
“Kencana <kesuma.kencana@yahoo.com>
2017-12-05 22:52:57 +0800

    feat: add in heroku setup

--- gulpfile.js
+++ gulpfile.js
@@ -49,14 +49,6 @@ gulp.task('scripts', () => {
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(reload({ stream: true }));
-
-  // return gulp.src('app/scripts/**/*.js')
-  //   .pipe($.plumber())
-  //   .pipe($.if(dev, $.sourcemaps.init()))
-  //   .pipe($.babel())
-  //   .pipe($.if(dev, $.sourcemaps.write('.')))
-  //   .pipe(gulp.dest('.tmp/scripts'))
-  //   .pipe(reload({stream: true}));
 });
 
 function lint(files) {
@@ -91,19 +83,19 @@ gulp.task('html', ['styles', 'scripts'], () => {
       removeScriptTypeAttributes: true,
       removeStyleLinkTypeAttributes: true
     })))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/public'));
 });
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
     .pipe($.cache($.imagemin()))
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest('dist/public/images'));
 });
 
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
     .concat('app/fonts/**/*'))
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
+    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/public/fonts')));
 });
 
 gulp.task('extras', () => {
@@ -112,10 +104,10 @@ gulp.task('extras', () => {
     '!app/*.html'
   ], {
     dot: true
-  }).pipe(gulp.dest('dist'));
+  }).pipe(gulp.dest('dist/public'));
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task('clean', del.bind(null, ['.tmp', 'dist/public']));
 
 gulp.task('serve', () => {
   runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {
@@ -148,7 +140,7 @@ gulp.task('serve:dist', ['default'], () => {
     notify: false,
     port: 9000,
     server: {
-      baseDir: ['dist']
+      baseDir: ['dist/public']
     }
   });
 });
@@ -189,7 +181,7 @@ gulp.task('wiredep', () => {
 });
 
 gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+  return gulp.src('dist/public/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
 gulp.task('default', () => {
